189fce71099e7bccaa3d0d959f1416a1fe083ccd
rdimaio <roccodimaio@yahoo.com>
2019-08-13 22:07:37 -0400

    Modified C++ Class AITankController -> Added a function called GetPlayerController to identify the player pawn, checked that player pawn was found,  and then output player pawn name to the log

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "GameFramework/Actor.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No TankAIController on player tank !"), *(GetOwner()->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player %s !"), *(PlayerTank->GetName()));
+	}
+
+}
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+
+	return Cast<ATank>(PlayerPawn);
+	
+}
+
+
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -4,8 +4,23 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); 
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No TankPlayerController on %s !"), *(GetOwner()->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController possessing %s !"), *(ControlledTank->GetName()));
+	}
+	
+}
 ATank* ATankPlayerController::GetControlledTank() const
 {
-	return Cast<ATank>(GetPawn());
+	return Cast<ATank>(GetPawn()); 
 }
 
